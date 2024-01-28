5f9e19f9d0bfc6e530d7f7292bf803775faf4018
abdellahhn <abdellahhanane44@gmail.com>
2024-01-27 19:36:07 -0500

    feat: Ajout des methodes dans la classe Restaurateur

--- src/main/java/ca/ulaval/glo2003/domain/Restaurateur.java
+++ src/main/java/ca/ulaval/glo2003/domain/Restaurateur.java
@@ -1,16 +1,18 @@
 package ca.ulaval.glo2003.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import jakarta.ws.rs.NotFoundException;
+
+import java.util.*;
 
 public class Restaurateur {
     private final String ownerId;
     private List<Restaurant> restaurants;
+    private Map<String, Restaurant> restaurantIdMap;
 
-    public Restaurateur(String ownerId) {
+    public Restaurateur() {
         this.ownerId = UUID.randomUUID().toString();
         this.restaurants = new ArrayList<>();
+        this.restaurantIdMap = new HashMap<>();
     }
 
     public String getOwnerId() {
@@ -22,12 +24,27 @@ public class Restaurateur {
     }
 
     public void addRestaurant(Restaurant restaurant) {
-//        restaurants.add(restaurant);
-        if (!restaurants.contains(restaurant)) {
+        if (!restaurantIdMap.containsKey(restaurant.getId())) {
             restaurants.add(restaurant);
+            restaurantIdMap.put(restaurant.getId(), restaurant);
         } else {
-            //maybe validate the id before adding?
-            throw new IllegalArgumentException("Un restaurant avec le meme ID existe deja");
+            throw new IllegalArgumentException("Un restaurant avec le meme Id existe deja");
+        }
+    }
+
+    public Restaurant getRestaurantById(String restaurantId) {
+        if (restaurantIdMap.containsKey(restaurantId)) {
+            return restaurantIdMap.get(restaurantId);
+        } else {
+            throw new NotFoundException("Restaurant introuvable avec le ID fourni: " + restaurantId);
+        }
+    }
+
+    private Restaurateur getRestaurateurById(String ownerId, Map<String, Restaurateur> restaurateurMap) {
+        if (restaurateurMap.containsKey(ownerId)) {
+            return restaurateurMap.get(ownerId);
+        } else {
+            throw new NotFoundException("Restaurateur introuvable avec le ID fourni: " + ownerId);
         }
     }
 }
