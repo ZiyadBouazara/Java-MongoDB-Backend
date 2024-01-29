0ddddf4351e17455ffe9a4f03b623f2c8d088a50
Nadir Berrezouk <naber259@ulaval.ca>
2024-01-28 21:00:04 -0500

    feat: list owner restaurant

--- src/main/java/ca/ulaval/glo2003/domain/ResourcesHandler.java
+++ src/main/java/ca/ulaval/glo2003/domain/ResourcesHandler.java
@@ -1,8 +1,11 @@
 package ca.ulaval.glo2003.domain;
 
+import ca.ulaval.glo2003.api.RestaurantResponse;
 import jakarta.ws.rs.NotFoundException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ResourcesHandler {
@@ -23,4 +26,15 @@ public class ResourcesHandler {
         if (restaurant == null) throw new NotFoundException(); // the restaurant does not exist
         return restaurant;
     }
+
+    public List<RestaurantResponse> getAllRestaurantsForOwner(String ownerId) {
+        List<RestaurantResponse> ownerRestaurants = new ArrayList<>();
+        for (Restaurant restaurant : restaurants.values()) {
+            if (restaurant.getOwnerId().equals(ownerId)) {
+                ownerRestaurants.add(new RestaurantResponse(restaurant));
+            }
+        }
+        return ownerRestaurants;
+    }
+
 }
