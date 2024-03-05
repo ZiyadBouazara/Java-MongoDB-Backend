779226731cb5a65a02b82f2141c313479746ce84
ZiyadBouazara <ziyad.bouazara1@ulaval.ca>
2024-03-05 12:27:07 -0500

    fix: merge conflict

--- src/main/java/ca/ulaval/glo2003/controllers/RestaurantResource.java
+++ src/main/java/ca/ulaval/glo2003/controllers/RestaurantResource.java
@@ -2,6 +2,7 @@ package ca.ulaval.glo2003.controllers;
 
 import ca.ulaval.glo2003.Main;
 import ca.ulaval.glo2003.domain.reservation.Reservation;
+import ca.ulaval.glo2003.domain.restaurant.ReservationConfiguration;
 import ca.ulaval.glo2003.domain.restaurant.Restaurant;
 import ca.ulaval.glo2003.domain.exceptions.InvalidParameterException;
 import ca.ulaval.glo2003.domain.exceptions.MissingParameterException;
@@ -24,16 +25,19 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
 
 import java.net.URI;
+import java.time.LocalTime;
+import java.time.Duration;
 import java.util.List;
 
 import static ca.ulaval.glo2003.models.RestaurantRequest.verifyRestaurantOwnership;
+
 @Path("/")
 public class RestaurantResource {
-    private ResourcesHandler resourcesHandler;
+    public ResourcesHandler resourcesHandler;
     private RestaurantFactory restaurantFactory;
 
-    public RestaurantResource() {
-        this.resourcesHandler = new ResourcesHandler();
+    public RestaurantResource(ResourcesHandler resourcesHandler) {
+        this.resourcesHandler = resourcesHandler;
         this.restaurantFactory = new RestaurantFactory();
     }
 
@@ -74,7 +78,12 @@ public class RestaurantResource {
     @POST
     @Path("restaurants/{id}/reservations")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createReservation(@PathParam("id") String restaurantId, ReservationRequest reservationRequest) {
+    public Response createReservation(@PathParam("id") String restaurantId, ReservationRequest reservationRequest)
+        throws NotFoundException, InvalidParameterException, MissingParameterException {
+        verifyValidRestaurantIdPath(restaurantId);
+        reservationRequest.verifyParameters();
+        reservationRequest.adjustReservationStartTime();
+        verifyValidReservationEndTime(reservationRequest, restaurantId);
         Reservation reservation = new Reservation(
             restaurantId,
             reservationRequest.getDate(),
@@ -99,9 +108,40 @@ public class RestaurantResource {
         return Response.ok(new ReservationResponse(reservation, resourcesHandler)).build();
     }
 
+    private void verifyValidRestaurantIdPath(String restaurantId) {
+        if (resourcesHandler.getRestaurant(restaurantId) == null) {
+            throw new NotFoundException("Restaurant not found with ID: " + restaurantId);
+        }
+    }
+
     private void verifyMissingHeader(String ownerId) throws MissingParameterException {
         if (ownerId == null) {
             throw new MissingParameterException("Missing 'Owner' header");
+        }
+    }
+
+    private void verifyValidReservationEndTime(ReservationRequest reservationRequest, String restaurantId)
+        throws InvalidParameterException {
+        Restaurant restaurant = resourcesHandler.getRestaurant(restaurantId);
+        ReservationConfiguration reservationConfiguration = restaurant.getRestaurantConfiguration();
+        LocalTime reservationEndTime = calculateReservationEndTime(reservationRequest, reservationConfiguration);
+        LocalTime closingTime = LocalTime.parse(restaurant.getHours().getClose());
+        LocalTime openingTime = LocalTime.parse(restaurant.getHours().getOpen());
+        verifyReservationWithinOperatingHours(reservationEndTime, closingTime, openingTime);
+    }
+
+    private LocalTime calculateReservationEndTime(
+        ReservationRequest reservationRequest, ReservationConfiguration reservationConfiguration) {
+        LocalTime reservationStartTime = LocalTime.parse(reservationRequest.getStartTime());
+        Duration reservationDuration = Duration.ofMinutes(reservationConfiguration.getDuration());
+        return reservationStartTime.plus(reservationDuration);
+    }
+
+    private void verifyReservationWithinOperatingHours(
+        LocalTime reservationEndTime, LocalTime closingTime, LocalTime openingTime) throws InvalidParameterException {
+        if (reservationEndTime.isAfter(closingTime) || reservationEndTime.isBefore(openingTime)) {
+            throw new InvalidParameterException(
+                "Invalid reservation start time, the reservation exceeds the restaurant's closing time");
         }
     }
 }
