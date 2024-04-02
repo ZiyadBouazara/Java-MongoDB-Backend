57425d7762582f1b65b9f04df82cea372c95608b
ZiyadBouazara <ziyad.bouazara1@ulaval.ca>
2024-04-02 00:40:18 -0400

    feat: Env Var PORT added

--- src/main/java/ca/ulaval/glo2003/Main.java
+++ src/main/java/ca/ulaval/glo2003/Main.java
@@ -14,9 +14,12 @@ import org.glassfish.jersey.server.ResourceConfig;
 import java.net.URI;
 
 public class Main {
-    public static final String BASE_URI = "http://0.0.0.0:8080/";
+    public static String BASE_URI = "http://0.0.0.0:";
 
     public static HttpServer startServer() {
+        final String port = getServerPort();
+        BASE_URI = String.format("%s%s/", BASE_URI, port);
+
         final ResourceConfig rc = new ResourceConfig()
             .register(new ApplicationBinder())
             .register(HealthResource.class)
@@ -27,6 +30,21 @@ public class Main {
             .register(NotFoundExceptionMapper.class);
 
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static String getServerPort() {
+        String portString = System.getenv("PORT");
+        int port = 8080;
+
+        if (portString != null) {
+            try {
+                port = Integer.parseInt(portString);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid port format in environment variable PORT. Using default port 8080.");
+            }
+        }
+
+        return String.valueOf(port);
     }
 
     public static void main(String[] args) {
