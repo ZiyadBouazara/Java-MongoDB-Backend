4bf3c30e879862b73cc8548556dc7302e0247e74
Znjis <ilyes2011@live.ca>
2024-04-09 19:09:49 -0400

    tests: 3 new tests, all involving creation

--- src/test/java/ca/ulaval/glo2003/service/ReservationServiceTest.java
+++ src/test/java/ca/ulaval/glo2003/service/ReservationServiceTest.java
@@ -15,12 +15,13 @@ import ca.ulaval.glo2003.domain.restaurant.Restaurant;
 import ca.ulaval.glo2003.domain.utils.Hours;
 import ca.ulaval.glo2003.service.assembler.CustomerAssembler;
 import ca.ulaval.glo2003.service.validators.ReservationValidator;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ReservationServiceTest {
 //    private static final String CUSTOMER_NAME = "John Deer";
@@ -34,31 +35,27 @@ public class ReservationServiceTest {
     private static final String START_TIME = "20:46:00";
     private static final int GROUP_SIZE = 3;
 
-
-    @Mock
-    private ReservationRepository reservationRepository;
     @Mock
     private RestaurantRepository restaurantRepository;
     @Mock
+    private ReservationRepository reservationRepository;
+    @Mock
     private ReservationFactory reservationFactory;
     @Mock
-    private ReservationValidator reservationValidator;
     private CustomerAssembler customerAssembler;
+    private ReservationValidator reservationValidator;
     private ReservationResponseAssembler reservationResponseAssembler;
     private ReservationGeneralResponseAssembler reservationGeneralResponseAssembler;
-    private ReservationService reservationService;
     private Restaurant restaurant;
-    private Reservation reservation;
-    private Customer customer;
+    @InjectMocks
+    private ReservationService reservationService;
 
     @BeforeEach
     public void setUp() {
-        customer = mock(Customer.class);
-        restaurant = new Restaurant(OWNER_ID, RESTO_NAME, RESTO_CAPACITY, RESTO_HOURS);
-        reservation = new Reservation(restaurant.getId(), DATE, START_TIME, GROUP_SIZE, customer);
-        customerAssembler = new CustomerAssembler();
-        reservationResponseAssembler = new ReservationResponseAssembler();
+        reservationValidator = new ReservationValidator();
         reservationGeneralResponseAssembler = new ReservationGeneralResponseAssembler();
+        reservationResponseAssembler = new ReservationResponseAssembler();
+        MockitoAnnotations.openMocks(this);
         reservationService = new ReservationService(
                 restaurantRepository,
                 reservationRepository,
@@ -69,72 +66,40 @@ public class ReservationServiceTest {
                 reservationGeneralResponseAssembler);
     }
 
-    public void givenValidRestaurantIdAndReservationRequest_whenCreatingReservation_shouldReturnRightReservationId()  {
-        ReservationRequest reservationRequestFixture = new ReservationRequestFixture().create();
-        when(restaurantRepository.findRestaurantById(restaurant.getId())).thenReturn(restaurant);
-        when(reservationFactory.createReservation(restaurant.getId(),
-                reservationRequestFixture.date(),
-                reservationRequestFixture.startTime(),
-                reservationRequestFixture.groupSize(),
-                customer))
-                .thenReturn(reservation);
 
-        //String createdId = reservationService.createReservation(restaurant.getId(), reservationRequestFixture);
+    @Test
+    public void givenValidReservationRequest_createReservation_shouldCreateAndSaveReservation() throws Exception {
+        String restaurantId = "restaurant1";
+        restaurant = new Restaurant(OWNER_ID, RESTO_NAME, RESTO_CAPACITY, RESTO_HOURS);
+        ReservationRequest reservationRequest = new ReservationRequestFixture().create();
+        when(restaurantRepository.findRestaurantById(restaurantId)).thenReturn(restaurant);
+        Customer mockCustomer = mock(Customer.class);
+        when(customerAssembler.fromDTO(reservationRequest.customer())).thenReturn(mockCustomer);
+        Reservation mockReservation = mock(Reservation.class);
+        when(reservationFactory.createReservation(any(), any(), any(), any(), any())).thenReturn(mockReservation);
 
-        //Assertions.assertThat(createdId).isEqualTo(TOURNAMENT_ID);
+        String reservationId = reservationService.createReservation(restaurantId, reservationRequest);
+
+        verify(reservationRepository).saveReservation(mockReservation);
     }
 
+    @Test
+    public void givenMissingGroupSize_createReservation_shouldThrowMissingParameterException() {
+        String restaurantId = "restaurant1";
+        ReservationRequest requestWithoutDate = new ReservationRequestFixture().createWithMissingDate();
 
-    //    //TODO : IMPLEMENT THE SERVICE TEST
-//    private static final String CUSTOMER_NAME = "John Deer";
-//    private static final String CUSTOMER_EMAIL = "john.deer@gmail.com";
-//    private static final String CUSTOMER_PHONE_NUMBER = "1234567890";
-//    private static final String OWNER_ID = "1";
-//    private static final String RESTO_NAME = "1";
-//    private static final Integer RESTO_CAPACITY = 10;
-//    private static final Hours RESTO_HOURS = new Hours("10:00:00", "21:00:00");
-//    Customer customer;
-//    ReservationService reservationService;
-//    @Mock
-//    ReservationFactory reservationFactory;
-//    @Mock
-//    RestaurantAndReservationRepository restaurantAndReservationRepository;
-//    @Mock
-//    CustomerAssembler customerAssembler;
-//    @Mock
-//    ReservationResponseAssembler reservationResponseAssembler;
-//    @Mock
-//    ReservationValidator reservationValidator;
-//    @Mock
-//    Restaurant restaurant;
-//
-//    CustomerDTO customerDto;
-//
-//    @BeforeEach
-//    void setup() {
-//        customerDto = new CustomerDTO(CUSTOMER_NAME, CUSTOMER_EMAIL, CUSTOMER_PHONE_NUMBER);
-//        customer = new Customer(CUSTOMER_NAME, CUSTOMER_EMAIL, CUSTOMER_PHONE_NUMBER);
-//        restaurant = new Restaurant(OWNER_ID, RESTO_NAME, RESTO_CAPACITY, RESTO_HOURS);
-//
-//        reservationService = new ReservationService(
-//                restaurantAndReservationRepository,
-//            restaurantRepository, reservationRepository, reservationFactory,
-//                customerAssembler,
-//                reservationResponseAssembler,
-//                reservationValidator
-//        );
-//    }
-//
-//    @Test
-//    void canCreateReservation() {
-//        // TODO: since we changed the flow, the service now accepts a request; We have to change the test.
-//        String date = "2024-03-31";
-//        String startTime = "20:46:00";
-//        Integer groupSize = 5;
-//        Reservation reservation = new Reservation(restaurant.getId(), date, startTime, groupSize, customer);
-//        when(restaurantAndReservationRepository.findRestaurantByRestaurantId(restaurant.getId())).thenReturn(restaurant);
-//        when(reservationFactory.createReservation(restaurant.getId(), date, startTime, groupSize, customer)).thenReturn(reservation);
-//
-//        //reservationService.createReservation(restaurant.getId(), date, startTime, groupSize);
-//    }
+        Assertions.assertThrows(MissingParameterException.class, () ->
+                reservationService.createReservation(restaurantId, requestWithoutDate));
+    }
+
+    @Test
+    public void givenExceedingGroupSize_createReservation_shouldThrowInvalidParameterException() {
+        restaurant = new Restaurant(OWNER_ID, RESTO_NAME, 1, RESTO_HOURS);
+        String restaurantId = "restaurant1";
+        ReservationRequest requestWithoutDate = new ReservationRequestFixture().withGroupSize(2).create();
+        when(restaurantRepository.findRestaurantById(restaurantId)).thenReturn(restaurant);
+
+        Assertions.assertThrows(InvalidParameterException.class, () ->
+                reservationService.createReservation(restaurantId, requestWithoutDate));
+    }
 }
